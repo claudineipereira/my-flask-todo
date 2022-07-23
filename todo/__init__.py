c06148ec8dae2e4203174140c551938021eda60f
Claudinei Pereira <contato@claudineipereira.com>
2022-07-23 17:58:40 -0300

    Use template & add tasks

--- todo/__init__.py
+++ todo/__init__.py
@@ -1,7 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
+db = []
+
+@app.route('/', methods=['GET', 'POST'])
 def home():
-    return '<h1>My Simple To-Do</h1>'
+    if request.method == 'POST':
+        task = request.form['task']
+        if len(task) > 0 and task not in db:
+            db.append(task)
+            # print(db.index(task))
+    return render_template('index.html', tasks=db)
