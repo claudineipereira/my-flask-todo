9326552d45cd12e2a0569d2485054910b3802538
Claudinei Pereira <contato@claudineipereira.com>
2022-07-23 22:08:20 -0300

    Add sqlite database

--- todo/__init__.py
+++ todo/__init__.py
@@ -1,20 +1,51 @@
 from flask import Flask, render_template, request, url_for, redirect
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-db = []
+db = SQLAlchemy(app)
+
+
+class Tasks(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(255), unique=True)
+
+    def __repr__(self):
+        return '<Task %r>' % self.id
+
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
+    tasks = Tasks.query.all()
+
     if request.method == 'POST':
         task = request.form['task']
-        if len(task) > 0 and task not in db:
-            db.append(task)
-            # print(db.index(task))
-    return render_template('index.html', tasks=db)
 
-@app.route('/delete/<task>')
-def delete(task):
-    task_idx = db.index(task)
-    db.pop(task_idx)
+        if len(task) > 0:
+            task_content = Tasks(content=task)
+
+            try:
+                db.session.add(task_content)
+                db.session.commit()
+            except:
+                db.session.rollback()
+                return redirect(url_for('home'))
+
+        return redirect(url_for('home'))
+
+    return render_template('index.html', tasks=tasks)
+
+@app.route('/delete/<id>')
+def delete(id):
+    task = Tasks.query.get_or_404(id)
+
+    try:
+        db.session.delete(task)
+        db.session.commit()
+    except:
+        db.session.rollback()
+        raise
+
     return redirect(url_for('home'))
